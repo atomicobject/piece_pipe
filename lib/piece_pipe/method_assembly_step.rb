467ac6b0c9c3684225e7a600286b33988d658361
David Crosby <dcrosby42@gmail.com>
2012-06-07 08:09:47 -0400

    Code focs for MethodAssemblyStep

--- lib/piece_pipe/method_assembly_step.rb
+++ lib/piece_pipe/method_assembly_step.rb
@@ -1,4 +1,16 @@
 module PiecePipe
+  # Essentially the same thing as MethodStep with AssemblyStep semantics.
+  #
+  # Because we don't have a clever way to determine that you might want to create
+  # an assembly line out of wrapped methods, you need to use the conveneince method Pipeline#assembly_step, eg.
+  #
+  #   PiecePipe::Pipeline.new.assembly_step(:add_seatbelts)
+  #
+  # Currently only supports arity-1 methods: the parameter will be the inputs Hash,
+  # the return is expected to be a Hash that will be produced via AssemblyStep#install.
+  #
+  # TODO; add support for arity-2 method wrapping to allow for zero-or-more #intall calls
+  #
   class MethodAssemblyStep < AssemblyStep
     def initialize(meth)
       @method = meth
