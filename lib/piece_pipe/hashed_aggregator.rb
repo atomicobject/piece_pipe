90862a1603545959fc446488962ac2f5b8113879
David Crosby <dcrosby42@gmail.com>
2012-06-07 08:21:23 -0400

    MapStep and HashedAggregator docs

--- lib/piece_pipe/hashed_aggregator.rb
+++ lib/piece_pipe/hashed_aggregator.rb
@@ -1,4 +1,16 @@
 module PiecePipe
+  #
+  # This Step acts like a sink: it will process ALL available inputs until the stream ends,
+  # and THEN call #aggregrate multiple times to transform accumulated data and produce output.
+  #
+  # For every key-value pair that arrives as an input (see MapStep for generating key-value pairs),
+  # an array of values is accumulated per key.
+  #
+  # When all inputs have been collected, #aggregate is called for each key that has been seen to-date.
+  #
+  # Your job is to implement #aggregate, perform some calculation on the key and its accumulated
+  # values, and generate output via zero or more calls to #produce.
+  #
   class HashedAggregator < Step
     def initialize
       @hash = {}
@@ -19,6 +31,8 @@ module PiecePipe
       @hash[item[:key]] << item[:value]
     end
 
+    # Accepts a key and an array of all values that have been collected for that key.
+    # Default impl simply produces { :key => key, :value => values } (essentially a noop).
     def aggregate(key, values)
       produce key: key, values: values 
     end
--- lib/piece_pipe/map_step.rb
+++ lib/piece_pipe/map_step.rb
@@ -1,4 +1,23 @@
 module PiecePipe
+  #
+  # This type of Step is designed to produce key-value pairs in anticipation of
+  # a following aggregation step (see HashedAggregator).
+  #
+  # Subclasses implement #map and can produce output via #emit(key, value).
+  # Zero-or-more pairs may be generted per #map invocation.
+  #
+  # The output structure is in fact a Hash with two keys: :key and :value, eg
+  #
+  #   class CalorieCounter < PiecePipe::MapStep
+  #     # Expects inputs to be a Hash with :person, :date and :calories.
+  #     # Output will look like: { :key => ["Dave","2012-05-10"], :value => 6000 }
+  #     def map(inputs)
+  #       key = [inputs[:person], inputs[:date]]
+  #       val = inputs[:calories]
+  #       emit key, val
+  #     end
+  #   end
+  # 
   class MapStep < Step
     def process(item)
       map item
